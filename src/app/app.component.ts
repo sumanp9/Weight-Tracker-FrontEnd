73f88d96a4fe45f533c5f2393cf4160f823644b3
sumanp9 <11.sumanpradhan@gmail.com>
2020-07-08 13:31:09 -0500

    Added conditions for login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private router: Router) {
+  }
   title = 'Weight-Tracker-FrontEnd';
+  logged = false;
 
   signin() {
       // Create a login UI
+    this.logged= true;
+    this.router.navigateByUrl('/trackerApp');
 
   }
 }
